5f61f30f5e3c8e9da7045cba11ff3c53a3d9ec00
Vyacheslav Shulika <wildhunter.85@yandex.ru>
2024-06-05 23:27:57 +0300

    fix_error

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -6,5 +6,5 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<slug:category_slug/', views.category_posts, 
-        name='category_posts category'),
+         name='category_posts category'),
     path('<int:id>/', views.post_detail, name='detail'), ]
