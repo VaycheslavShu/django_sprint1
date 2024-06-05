cb2a17a0eade6128851b5fbb6aa8119dffcdbded
Vyacheslav Shulika <wildhunter.85@yandex.ru>
2024-06-05 23:09:26 +0300

    fix_error pep

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -5,5 +5,6 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<slug:category_slug/', views.category_posts, name='category_posts category'),
+    path('<slug:category_slug/', views.category_posts, 
+        name='category_posts category'),
     path('<int:id>/', views.post_detail, name='detail'), ]
