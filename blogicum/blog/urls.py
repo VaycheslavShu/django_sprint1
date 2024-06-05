35bb87ad8ae97afc0ca79cdcb205458c98b975af
Vyacheslav Shulika <wildhunter.85@yandex.ru>
2024-06-05 14:13:27 +0300

    rename_views

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -5,5 +5,5 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<slug:category_slug/', views.category_posts, name='category'),
-    path('<int:id>/', views.post_detail, name='detail'), ]
+    path('<slug:category_slug/', views.category_posts, name='category_post'),
+    path('<int:id>/', views.post_detail, name='post_detail'), ]
