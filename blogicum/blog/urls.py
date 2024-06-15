2216ee332133453b1e6ca9a657d8f2ad22429e8a
Vyacheslav Shulika <wildhunter.85@yandex.ru>
2024-06-16 00:30:08 +0300

    fix_bug_pep_error,

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -7,4 +7,4 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:id>/', views.post_detail, name='post_detail'),
     path('<slug:category_slug>/', views.category_posts,
-         name='category_posts'),]
+         name='category_posts'), ]
