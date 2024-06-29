2f9ac4a12d04c5dfe5644e2bc9afccb453dbf977
Vyacheslav Shulika <wildhunter.85@yandex.ru>
2024-06-29 23:44:42 +0300

    fix_error namespace post_detail

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -8,4 +8,4 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:id>/', views.post_detail, name='post_detail'),
     path('<slug:category_slug>/', views.category_posts,
-         name='category'), ]
+         name='category_posts'), ]
